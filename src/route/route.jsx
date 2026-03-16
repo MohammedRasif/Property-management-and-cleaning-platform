53b64b8dd452b1c1897cdee4ac864eb012a41a08
Shawon Reza <shawon.reza.sr81@gmail.com>
2026-03-16 11:52:23 +0600

    first commit

---
+++ src/route/route.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router";
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>asdkh</div>,
+    },
+]);
+
+export default router;
